109a450aba32f7a4a70ce393e298ff5c554a8748
Naim Ahmed <naimahmod852@gmail.com>
2023-07-16 21:12:23 +0600

    edit env

--- src/config/secret.ts
+++ src/config/secret.ts
@@ -4,10 +4,7 @@ import path from 'path'
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-  env:'production',
-  port: 7000,
-  db_url: 'mongodb+srv://shofystore22:Amp0QXX2UGFrMQJ6@cluster0.8pmmysm.mongodb.net/redux-assignment'
-  // env: process.env.NODE_ENV,
-  // port: process.env.PORT,
-  // db_url: process.env.DATABASE_URL
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  db_url: process.env.DATABASE_URL
 }

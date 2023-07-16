f92b822947b581a74258b82d2b7dda98effe5a0b
Naim Ahmed <naimahmod852@gmail.com>
2023-07-16 18:21:08 +0600

    added env

--- dist/config/secret.js
+++ dist/config/secret.js
@@ -7,7 +7,10 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
 dotenv_1.default.config({ path: path_1.default.join(process.cwd(), '.env') });
 exports.default = {
-    env: process.env.NODE_ENV,
-    port: process.env.PORT,
-    db_url: process.env.DATABASE_URL
+    env: 'production',
+    port: 7000,
+    db_url: 'mongodb+srv://shofystore22:Amp0QXX2UGFrMQJ6@cluster0.8pmmysm.mongodb.net/redux-assignment'
+    // env: process.env.NODE_ENV,
+    // port: process.env.PORT,
+    // db_url: process.env.DATABASE_URL
 };
--- src/config/secret.ts
+++ src/config/secret.ts
@@ -4,7 +4,10 @@ import path from 'path'
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT,
-  db_url: process.env.DATABASE_URL
+  env:'production',
+  port: 7000,
+  db_url: 'mongodb+srv://shofystore22:Amp0QXX2UGFrMQJ6@cluster0.8pmmysm.mongodb.net/redux-assignment'
+  // env: process.env.NODE_ENV,
+  // port: process.env.PORT,
+  // db_url: process.env.DATABASE_URL
 }
